67a3127202b0038a84394285b6e0d5d142be2126
Diamond <dbrown@pursuit.org>
2024-07-08 14:58:39 -0400

    Navbar styling complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/transaction">
-      <h1>Budget App</h1>
+    <nav className='navbar'>
+      <Link className='app-title-link' to="/transaction">
+      <h1 className='app-title'>Budget App</h1>
       </Link>
-        <Link to='/transaction/new'>
-        <button>NEW TRANSACTION</button>
+        <Link className='new-transaction-button-link' to='/transaction/new'>
+        <button className='new-transaction-button'>NEW TRANSACTION</button>
         </Link>
     </nav>
   )
